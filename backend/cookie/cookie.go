ba771b695089fc38dc1ed8949058df3bd41f0c34
Destructzz <sklyarenko_2006@mail.ru>
2025-04-22 18:18:56 +0300

    cookie dir

---
+++ backend/cookie/cookie.go
@@ -0,0 +1,7 @@
+package cookie;
+
+func GenerateToken(); // 45< symbols
+
+func SetCookie(); // by http.SetCookie - кладет в request токен в куки
+
+func GetCookie(); // берет токен из cookie либо ошибку
